e02ea029228b6892fb2babffadcdc10ab7061279
Andi07SH <andi.shehi07@gmail.com>
2025-11-07 19:47:32 +0100

    Revert "creating entity Student"
    This reverts commit e09b04264ff3c887802e25d9e6ada10772706a21.

--- src/main/java/com/system/studentmanagementSystem/StudentmanagementSystemApplication.java
+++ src/main/java/com/system/studentmanagementSystem/StudentmanagementSystemApplication.java
@@ -3,14 +3,11 @@ package com.system.studentmanagementSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class StudentmanagementSystemApplication {
 
-    public static void main(String[] args) {
-
-
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementSystemApplication.class, args);
+	}
 
 }
