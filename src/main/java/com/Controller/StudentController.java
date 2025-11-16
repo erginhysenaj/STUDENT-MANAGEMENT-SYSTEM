bfc7f9eb38734e3a2e1d2e8186883f1824fb272c
Andi07SH <andi.shehi07@gmail.com>
2025-11-16 10:12:25 +0100

    accomodating angular interfae

--- src/main/java/com/Controller/StudentController.java
+++ src/main/java/com/Controller/StudentController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 
     private final StudentService studentService;
--- src/main/java/com/system/studentmanagementSystem/StudentmanagementSystemApplication.java
+++ src/main/java/com/system/studentmanagementSystem/StudentmanagementSystemApplication.java
@@ -2,8 +2,12 @@ package com.system.studentmanagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com"})
+@EnableJpaRepositories(basePackages = {"com.Repository"})
+@EntityScan(basePackages = {"com.Entity"})
 public class StudentmanagementSystemApplication {
 
 	public static void main(String[] args) {
